aa5a3e5b6e49256e9b874f3d3dba74aa1106cd09
Keiko Hisana Azka <khisanaazka@gmail.com>
2025-01-30 12:16:08 +0000

    p

--- lab_11/server.js
+++ lab_11/server.js
@@ -33,6 +33,11 @@ if(operator === "add"){
 
 app.use(express.static('public'))
 
+app.get('/getform', function(req, res){
+    var name = req.query.name;
+    var quest = req.query.quest;
+     res.send("Hi "+name+" I am sure you will "+quest) ;
+    });
 
 
 app.post('/postform', function(req, res){
