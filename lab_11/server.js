898b70fccb2ca6a6a60f082c3b798302fdb2c0cf
Keiko Hisana Azka <khisanaazka@gmail.com>
2025-01-30 12:14:03 +0000

    postform

--- lab_11/server.js
+++ lab_11/server.js
@@ -38,3 +38,12 @@ app.get('/getform', function(req, res){
     var quest = req.query.quest;
      res.send("Hi "+name+" I am sure you will "+quest) ;
     });
+
+
+app.post('/postform', function(req, res){
+var name = req.body.name;
+var quest = req.body.quest;
+ res.send("Hi "+name+" I am sure you will "+quest) ;
+});
+
+app.use(express.urlencoded({extended:true}))
