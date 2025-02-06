92a015254f44f5a8846ce04c1615e1f1d35bc77f
Keiko Hisana Azka <khisanaazka@gmail.com>
2025-02-06 11:43:49 +0000

    maybe fixxxxx

--- lab_12/server.js
+++ lab_12/server.js
@@ -18,7 +18,7 @@ var spotifyApi = new spotifyWebApi({
 });
 
 
-spotifyApi.clientCredentialsGranr().then(
+spotifyApi.clientCredentialsGrant().then(
     function(data){
         console.log('The access token expires in '+ data.body['expires_in']);
         console.log('The access token is '+ data.body['access_token']);
