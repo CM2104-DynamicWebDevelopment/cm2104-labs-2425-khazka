f8f8020edaa664ef370965578f7736b27b403dca
Keiko Hisana Azka <khisanaazka@gmail.com>
2025-02-13 11:55:21 +0000

    aaa

--- lab_13/server.js
+++ lab_13/server.js
@@ -64,3 +64,12 @@ app.get('/all', function(req, res) {
     });
    });
    
+   app.post('/update', function(req, res) {
+    var query = { quote: req.body.quote };
+    var newvalues = { $set: {name: req.body.newname, quote: req.body.newquote } };
+    db.collection('quotes').updateOne(query,newvalues, function(err, result) {
+    if (err) throw err;
+    res.redirect('/');
+    });
+   });
+   
