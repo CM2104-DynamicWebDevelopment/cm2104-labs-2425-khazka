966399247f9bb50c2e0d2319349874cb92927e4d
Keiko Hisana Azka <khisanaazka@gmail.com>
2025-03-13 12:16:05 +0000

    aaa

--- lab_16/server.js
+++ lab_16/server.js
@@ -18,3 +18,13 @@ io.on('connection', function (socket) {
 http.listen(8080, function(){
  console.log('listening on *:8080');
 });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+    console.log('user disconnected');
+    });
+    
+    socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    });
+   });
