d52775c2e262027c73668dac2d3099ecc1e6382f
Keiko Hisana Azka <khisanaazka@gmail.com>
2025-03-13 12:11:22 +0000

    aaa

--- lab_16/server.js
+++ lab_16/server.js
@@ -9,9 +9,12 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res){
  res.render('pages/index');
 });
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-   })
+    socket.on('disconnect', function () {
+    console.log('user disconnected');
+    });
+   });
 http.listen(8080, function(){
  console.log('listening on *:8080');
 });
